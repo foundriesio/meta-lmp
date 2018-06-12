c0daf31bc9e01ae4975fb14b6f514195066661f2
Stefan Agner <stefan.agner@toradex.com>
2018-06-12 17:18:21 +0200

    u-boot-ostree-scr: use distro boot variables by default
    Distro boot makes sure that the variables ${devtype} ${devnum} and
    ${distro_bootpart} are set according to the location where the boot
    script has been found. Make use of those variables by default.
    
    If a device happens not to provide them, it is probably not using
    the default distro boot mechanism and needs custom handling anyway.
    
    This allows to boot from internal eMMC, external SD card or from a
    USB mass storage device.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- recipes-bsp/u-boot/u-boot-ostree-scr/boot.cmd
+++ recipes-bsp/u-boot/u-boot-ostree-scr/boot.cmd
@@ -1,3 +1,9 @@
-fatload mmc 0:1 $loadaddr /uEnv.txt
+if test ${distro_bootpart} != 1
+then
+    echo "Boot partition needs to be the first partition"
+    exit
+fi
+
+fatload ${devtype} ${devnum}:1 $loadaddr /uEnv.txt
 env import -t $loadaddr $filesize
 run bootcmd

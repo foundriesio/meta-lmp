5468a3c3834baf8caf3b2f1bc97c4b0ac3b227eb
Ricardo Salveti <ricardo@foundries.io>
2019-08-20 17:42:59 -0300

    u-boot-ostree-scr-fit: add recipe for u-boot scr in fit format
    Boot script available in fit format for verified boot.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

---
+++ recipes-bsp/u-boot/u-boot-ostree-scr-fit/cubox-i/boot.cmd
@@ -0,0 +1,17 @@
+# Default boot type and device
+setenv devtype mmc
+setenv devnum ${mmcdev}
+
+load ${devtype} ${devnum}:2 ${loadaddr} /boot/loader/uEnv.txt
+env import -t ${loadaddr} ${filesize}
+
+load ${devtype} ${devnum}:2 ${loadaddr} "/boot"${kernel_image}
+
+# Apply OP-TEE overlay (provided by the OP-TEE OS)
+imxtract ${loadaddr}#conf@${fdtfile} fdt@${fdtfile} ${fdt_addr}
+fdt addr ${fdt_addr}
+fdt resize 0x1000
+# OP-TEE overlay available at a build-time known address
+fdt apply 0x18200000
+
+bootm ${loadaddr}#conf@${fdtfile} ${loadaddr}:ramdisk@1 ${fdt_addr}

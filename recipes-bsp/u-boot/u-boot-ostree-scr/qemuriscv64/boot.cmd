298fec3701346b1d4530a37bb81d7e3e2f82f436
Michael Scott <mike@foundries.io>
2019-09-15 21:58:28 -0700

    u-boot: qemuriscv64: copy QEMU loaded DTB to safe area (fdt_addr_r)
    Recent kernel commit in 5.3 changed page table setup which now
    crushes the QEMU loaded DTB:
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/arch/riscv/kernel?h=v5.3&id=671f9a3e2e24cdeb2d2856abee7422f093e23e29
    
    Let's copy the DTB to the established load addr (fdt_addr_r) instead
    where it's safe.
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- recipes-bsp/u-boot/u-boot-ostree-scr/qemuriscv64/boot.cmd
+++ recipes-bsp/u-boot/u-boot-ostree-scr/qemuriscv64/boot.cmd
@@ -4,6 +4,12 @@ then
     exit
 fi
 
+# Recent kernel commit in 5.3:
+# https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/arch/riscv/kernel?h=v5.3&id=671f9a3e2e24cdeb2d2856abee7422f093e23e29
+# changed page table setup which now crushes the qemu loaded DTB.
+# Let's copy the DTB to the established load addr instead where it's safe.
+cp ${fdt_addr} ${fdt_addr_r} 1000
+
 fatload ${devtype} ${devnum}:1 $scriptaddr /uEnv.txt
 env import -t $scriptaddr $filesize
 run bootcmd

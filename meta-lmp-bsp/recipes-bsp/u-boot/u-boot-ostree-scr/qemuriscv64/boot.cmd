7fd7d8291782e48c930be0dc416c221817a7943c
Ricardo Salveti <ricardo@foundries.io>
2020-01-08 16:01:22 -0300

    bsp: u-boot-ostree-scr: qemuriscv64: use fdtcontroladdr directly
    Use fdtcontroladdr directly instead of fdt_addr as it is
    available on both u-boot 2019.10 and 2020.01.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr/qemuriscv64/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr/qemuriscv64/boot.cmd
@@ -8,7 +8,7 @@ fi
 # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/arch/riscv/kernel?h=v5.3&id=671f9a3e2e24cdeb2d2856abee7422f093e23e29
 # changed page table setup which now crushes the qemu loaded DTB.
 # Let's copy the DTB to the established load addr instead where it's safe.
-cp ${fdt_addr} ${fdt_addr_r} 1000
+cp ${fdtcontroladdr} ${fdt_addr_r} 1000
 
 fatload ${devtype} ${devnum}:1 $scriptaddr /uEnv.txt
 env import -t $scriptaddr $filesize

ee4d91f7666435cb6abf139f8602c62e3d6779d8
Michael Scott <mike@foundries.io>
2020-05-14 12:53:50 -0700

    bsp: u-boot scripts: clear environment during rollback
    Refactor boot scripts to allow clearing the environment on a rollback.
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/beaglebone-yocto/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/beaglebone-yocto/boot.cmd
@@ -1,10 +1,10 @@
 # Default boot type and device
-setenv bootlimit 3
-
+setenv bootcmd_envinit 'setenv bootlimit 3'
 setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${addr_fit} "/boot"${kernel_image}'
 setenv bootcmd_run 'bootm ${addr_fit}#conf@${fdtfile}'
 setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
-setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
+setenv bootcmd_otenv 'ext4load ${devtype} ${devnum}:2 ${loadaddr} /boot/loader/uEnv.txt; env import -t ${loadaddr} ${filesize};'
+setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then env default -a; run bootcmd_envinit; run bootcmd_otenv; setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
 setenv bootostree 'run bootcmd_load_f; run bootcmd_run'
 setenv altbootcmd 'run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
 
@@ -17,8 +17,8 @@ setenv bootargs
 setenv kernel_image2
 setenv bootargs2
 
-ext4load ${devtype} ${devnum}:2 ${loadaddr} /boot/loader/uEnv.txt
-env import -t ${loadaddr} ${filesize}
+run bootcmd_envinit
+run bootcmd_otenv
 
 if test "${rollback}" = "1"; then run altbootcmd; else run bootostree; if test ! "${upgrade_available}" = "1"; then setenv upgrade_available 1; saveenv; fi; reset; fi
 

ee2940a5fdffcffd0fce6d4ab06642fafa930e92
Ricardo Salveti <ricardo@foundries.io>
2020-09-29 12:07:02 -0300

    bsp: apalis-imx8: boot.cmd: add logic to load hdmi firmware
    Add required logic to load the hdmi firmware during early boot, as done
    by toradex u-boot-distro-boot recipe, but in this case loading from a
    fitimage.
    
    Signed-off-by: Michael Scott <mike@foundries.io>
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/apalis-imx8/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/apalis-imx8/boot.cmd
@@ -6,18 +6,21 @@ echo "Using freescale_${fdt_file}"
 setenv bootlimit 3
 setenv devtype mmc
 setenv devnum ${mmcdev}
+setenv hdmi_image hdmitxfw.bin
 setenv m4_0_image core0_m4_image.bin
 setenv m4_1_image core1_m4_image.bin
 
+setenv bootcmd_boot_hdmi 'hdp load ${loadaddr}'
 setenv bootcmd_boot_m4_0 'dcache flush; bootaux ${loadaddr} 0'
 setenv bootcmd_boot_m4_1 'dcache flush; bootaux ${loadaddr} 1'
+setenv bootcmd_load_hdmi 'if imxtract ${ramdisk_addr_r}#conf@freescale_${fdt_file} loadable@${hdmi_image} ${loadaddr}; then run bootcmd_boot_hdmi; fi'
 setenv bootcmd_load_m4_0 'if imxtract ${ramdisk_addr_r}#conf@freescale_${fdt_file} loadable@${m4_0_image} ${loadaddr}; then run bootcmd_boot_m4_0; fi;'
 setenv bootcmd_load_m4_1 'if imxtract ${ramdisk_addr_r}#conf@freescale_${fdt_file} loadable@${m4_1_image} ${loadaddr}; then run bootcmd_boot_m4_1; fi;'
 setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${ramdisk_addr_r} "/boot"${kernel_image}'
 setenv bootcmd_run 'bootm ${ramdisk_addr_r}#conf@freescale_${fdt_file}'
 setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
 setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
-setenv bootostree 'run bootcmd_load_f; run bootcmd_load_m4_0; run bootcmd_load_m4_1; run bootcmd_run'
+setenv bootostree 'run bootcmd_load_f; run bootcmd_load_hdmi; run bootcmd_load_m4_0; run bootcmd_load_m4_1; run bootcmd_run'
 setenv altbootcmd 'run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
 
 if test ! -e ${devtype} ${devnum}:1 uboot.env; then saveenv; fi

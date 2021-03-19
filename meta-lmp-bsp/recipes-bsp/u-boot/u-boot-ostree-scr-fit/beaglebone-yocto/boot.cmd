371e2708912ec32305a7279150f8df7ce34af64c
Ricardo Salveti <ricardo@foundries.io>
2021-03-18 21:04:42 -0300

    bsp: u-boot-ostree-scr/fit: allow built time selection for fit node separator
    As an effort to move away from using unit address when loading the FIT
    components (u-boot CVE-2021-27138), allow the fit node separator to be
    set during build time via the FIT_NODE_SEPARATOR variable.
    
    This is required as the fit node separator from boot.cmd needs to be
    fully in sync with what is set in the kernel fit, otherwise the
    machine won't be able to boot the kernel fit.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/beaglebone-yocto/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/beaglebone-yocto/boot.cmd
@@ -4,7 +4,7 @@ setenv bootlimit 3
 setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
 setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:2 ${loadaddr} /boot/loader/uEnv.txt; env import -t ${loadaddr} ${filesize}'
 setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${addr_fit} "/boot"${kernel_image}'
-setenv bootcmd_run 'bootm ${addr_fit}#conf@${fdtfile}'
+setenv bootcmd_run 'bootm ${addr_fit}#conf@@FIT_NODE_SEPARATOR@@${fdtfile}'
 setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
 setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
 setenv bootostree 'run bootcmd_load_f; run bootcmd_run'
--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/rpi/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/rpi/boot.cmd
@@ -6,7 +6,7 @@ setenv devnum 0
 setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
 setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:2 ${scriptaddr} /boot/loader/uEnv.txt; env import -t ${scriptaddr} ${filesize}'
 setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${ramdisk_addr_r} "/boot"${kernel_image}'
-setenv bootcmd_run 'bootm ${ramdisk_addr_r}:kernel@1 ${ramdisk_addr_r}:ramdisk@1 ${fdt_addr}'
+setenv bootcmd_run 'bootm ${ramdisk_addr_r}:kernel@@FIT_NODE_SEPARATOR@@1 ${ramdisk_addr_r}:ramdisk@@FIT_NODE_SEPARATOR@@1 ${fdt_addr}'
 setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
 setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
 setenv bootostree 'run bootcmd_load_f; run bootcmd_run'

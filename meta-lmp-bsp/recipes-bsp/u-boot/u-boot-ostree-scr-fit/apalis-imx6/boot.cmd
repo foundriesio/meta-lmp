98a09293c218930d82675d129f3971eb0c23bf3a
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-06-01 19:12:46 +0300

    base: u-boot-ostree-scr-fit: use fdt_file_final
    1. Use fdt_file_final to cover the case when final fdt file name is
    created by concatenating different prefixes etc (like freescale_ for
    imx8m-based boards).
    2. Add support for loading non-changed DTB from FIT image.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/apalis-imx6/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/apalis-imx6/boot.cmd
@@ -1,7 +1,7 @@
 # Default to ixora-v1.1, which is the carrier board we officially support
 ## Careful when changing to another dtb as the mmcblk order might differ
-setenv fdt_file imx6q-apalis-ixora-v1.1.dtb
-echo "Using ${fdt_file_r}"
+setenv fdt_file_final imx6q-apalis-ixora-v1.1.dtb
+echo "Using ${fdt_file_final}"
 
 # Default boot type and device
 setenv bootlimit 3
@@ -12,5 +12,6 @@ setenv rootpart 2
 
 setenv fdt_addr 0x15800000
 setenv optee_ovl_addr 0x16000000
+setenv fit_addr ${loadaddr}
 
 @@INCLUDE_COMMON@@

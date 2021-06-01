3c4a55fc9e4396b9c3bdd99eadd1d461250c51f8
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-06-01 19:12:46 +0300

    base: u-boot-ostree-scr-fit: use fdt_file_final
    1. Use fdt_file_final to cover the case when final fdt file name is
    created by concatenating different prefixes etc (like freescale_ for
    imx8m-based boards).
    2. Add support for loading non-changed DTB from FIT image.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx6ullevk/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx6ullevk/boot.cmd
@@ -2,6 +2,8 @@
 run findfdt
 echo "Using ${fdt_file}"
 
+setenv fdt_file_final ${fdt_file}
+
 # Default boot type and device
 setenv bootlimit 3
 setenv devtype mmc
@@ -11,5 +13,6 @@ setenv rootpart 2
 
 setenv fdt_addr 85800000
 setenv optee_ovl_addr 0x86000000
+setenv fit_addr ${loadaddr}
 
 @@INCLUDE_COMMON@@

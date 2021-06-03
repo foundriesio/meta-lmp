63ebd92779003b7f82bc5daa19dada8eb5259cef
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-06-03 18:12:18 +0300

    u-boot-ostree-scr-fit: imx6ullevk: add defines for boot updates
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx6ullevk/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx6ullevk/boot.cmd
@@ -15,4 +15,15 @@ setenv fdt_addr 85800000
 setenv optee_ovl_addr 0x86000000
 setenv fit_addr ${loadaddr}
 
+# Boot firmware updates
+setenv bootloader 2
+setenv bootloader2 8a
+setenv bootloader_s 1002
+setenv bootloader2_s 108a
+setenv bootloader_image "SPL"
+setenv bootloader_s_image ${bootloader_image}
+setenv bootloader2_image "u-boot.itb"
+setenv bootloader2_s_image ${bootloader2_image}
+setenv uboot_hwpart 0
+
 @@INCLUDE_COMMON@@

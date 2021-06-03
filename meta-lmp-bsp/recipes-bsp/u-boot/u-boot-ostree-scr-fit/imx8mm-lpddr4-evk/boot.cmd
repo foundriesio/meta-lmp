e5ee047bc35ce12126a65eca4410132d5224537c
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-06-03 18:12:00 +0300

    u-boot-ostree-scr-fit: imx8mmevk: add defines for boot updates
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx8mm-lpddr4-evk/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx8mm-lpddr4-evk/boot.cmd
@@ -7,7 +7,19 @@ setenv devnum 2
 setenv bootpart 1
 setenv rootpart 2
 
+# Boot image files
 setenv fdt_file_final freescale_${fdt_file}
 setenv fit_addr ${initrd_addr}
+
+# Boot firmware updates
+setenv bootloader 42
+setenv bootloader2 300
+setenv bootloader_s 1042
+setenv bootloader2_s 1300
+setenv bootloader_image "imx-boot"
+setenv bootloader_s_image ${bootloader_image}
+setenv bootloader2_image "u-boot.itb"
+setenv bootloader2_s_image ${bootloader2_image}
+setenv uboot_hwpart 1
 
 @@INCLUDE_COMMON@@

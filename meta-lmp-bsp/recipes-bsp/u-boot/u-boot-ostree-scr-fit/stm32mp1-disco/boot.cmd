5773fbe2be373f78cc7d7051a10e0ab6fd02723d
Ricardo Salveti <ricardo@foundries.io>
2021-07-05 19:59:50 -0300

    bsp: u-boot-ostree-scr-fit: add support for stm32mp1-disco
    Add initial support for stm32mp1-disco, based on the same boot logic as
    currently set via lmp.cfg.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

---
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/stm32mp1-disco/boot.cmd
@@ -0,0 +1,23 @@
+echo "Using ${fdtfile}"
+
+# Default boot type and device
+setenv bootlimit 3
+setenv devtype ${boot_device}
+setenv devnum ${boot_instance}
+
+setenv loadaddr ${ramdisk_addr_r}
+
+setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
+setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:5 ${loadaddr} /boot/loader/uEnv.txt; env import -t ${loadaddr} ${filesize} kernel_image bootargs kernel_image2 bootargs2'
+setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:5 ${loadaddr} "/boot"${kernel_image}'
+setenv bootcmd_run 'bootm ${loadaddr}#conf-${fdtfile}'
+setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
+setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
+setenv bootostree 'run bootcmd_load_f; run bootcmd_run'
+setenv altbootcmd 'run bootcmd_otenv; run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
+
+env info -d -q; if test $? -eq 0; then saveenv; fi
+
+if test "${rollback}" = "1"; then run altbootcmd; else run bootcmd_otenv; run bootostree; if test ! "${upgrade_available}" = "1"; then setenv upgrade_available 1; saveenv; fi; reset; fi
+
+reset

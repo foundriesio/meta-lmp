f9258d8782d6fc1a8cba21599bea2c02cc33cfe2
Tim Anderson <tim.anderson@foundries.io>
2021-08-05 23:18:30 +0000

    bsp: u-boot-ostree-scr-fit: add support for imx8mp-lpddr4-evk
    - Add ostree compatible boot script for use with kernel fit-image
    
    Signed-off-by: Tim Anderson <tim.anderson@foundries.io>
    [mike@foundries.io: mx8mp has 0 offset for boot part]
    Signed-off-by: Michael Scott <mike@foundries.io>

---
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx8mp-lpddr4-evk/boot.cmd
@@ -0,0 +1,25 @@
+echo "Using freescale_${fdt_file}"
+
+# Default boot type and device
+setenv bootlimit 3
+setenv devtype mmc
+setenv devnum 2
+setenv bootpart 1
+setenv rootpart 2
+
+# Boot image files
+setenv fdt_file_final freescale_${fdt_file}
+setenv fit_addr ${initrd_addr}
+
+# Boot firmware updates
+setenv bootloader 0
+setenv bootloader2 300
+setenv bootloader_s 1000
+setenv bootloader2_s 1300
+setenv bootloader_image "imx-boot"
+setenv bootloader_s_image ${bootloader_image}
+setenv bootloader2_image "u-boot.itb"
+setenv bootloader2_s_image ${bootloader2_image}
+setenv uboot_hwpart 1
+
+@@INCLUDE_COMMON@@

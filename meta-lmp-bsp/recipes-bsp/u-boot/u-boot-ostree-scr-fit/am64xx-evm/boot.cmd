74d9a2e4a4081d0ca44316df38e27158c24e0eea
Ricardo Salveti <ricardo@foundries.io>
2021-09-03 14:15:00 -0300

    bsp: u-boot-ostree-scr-fit: add support for am64xx-evm
    Add scr-fit boot script support for am64xx-evm, based on ostree-scr.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

---
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/am64xx-evm/boot.cmd
@@ -0,0 +1,21 @@
+# Default boot type and device
+setenv bootlimit 3
+setenv devtype mmc
+setenv devnum ${mmcdev}
+
+setenv loadaddr 0x90000000
+
+setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
+setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:2 ${scriptaddr} /boot/loader/uEnv.txt; env import -t ${scriptaddr} ${filesize} kernel_image bootargs kernel_image2 bootargs2'
+setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${loadaddr} "/boot"${kernel_image}'
+setenv bootcmd_run 'bootm ${loadaddr}#conf-ti_${fdtfile}'
+setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
+setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
+setenv bootostree 'run bootcmd_load_f; run findfdt; run bootcmd_run'
+setenv altbootcmd 'run bootcmd_otenv; run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
+
+if test ! -e ${devtype} ${devnum}:1 uboot.env; then saveenv; fi
+
+if test "${rollback}" = "1"; then run altbootcmd; else run bootcmd_otenv; run bootostree; if test ! "${upgrade_available}" = "1"; then setenv upgrade_available 1; saveenv; fi; reset; fi
+
+reset

16b4af31e56eab64aeca30fcaa682aaf48cb9f9f
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-11-03 12:41:36 +0200

    bsp: u-boot-ostree-scr-fit: boot firmware updates for QSPI
    Add support of boot firmware updates on UltraZed QSPI.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/uz/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/uz/boot.cmd
@@ -20,15 +20,30 @@ setenv bootloader_s_image ${bootloader_image}
 setenv bootloader2_image "u-boot.itb"
 setenv bootloader2_s_image ${bootloader2_image}
 
-setenv update_primary_image 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} boot0001.bin ${filesize}';
-setenv update_secondary_image 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} boot0002.bin ${filesize}';
+if test "${modeboot}" = "qspiboot"; then
+	setenv qspi_bootloader_offset 0x0
+	setenv qspi_bootloader_s_offset 0x50000
+	setenv qspi_bootloader2_offset 0x100000
+	setenv qspi_bootloader2_s_offset 0xaa0000
 
-setenv update_primary_image2 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} u-boot0001.itb ${filesize}';
-setenv update_secondary_image2 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} u-boot0002.itb ${filesize}';
+	setenv update_primary_image 'echo "${fio_msg} writing ${image_path} ..."; sf probe; sf update ${loadaddr} ${qspi_bootloader_offset} ${filesize}';
+	setenv update_secondary_image 'echo "${fio_msg} writing ${image_path} ..."; sf probe; sf update ${loadaddr} ${qspi_bootloader_s_offset} ${filesize}';
+
+	setenv update_primary_image2 'echo "${fio_msg} writing ${image_path} ..."; sf probe; sf update ${loadaddr} ${qspi_bootloader2_offset} ${filesize}';
+	setenv update_secondary_image2 'echo "${fio_msg} writing ${image_path} ..."; sf probe; sf update ${loadaddr} ${qspi_bootloadea2_s_offset} ${filesize}';
+	setenv set_primary_boot "multi_boot 0"
+	setenv set_secondary_boot "multi_boot 10"
+else
+	setenv update_primary_image 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} boot0001.bin ${filesize}';
+	setenv update_secondary_image 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} boot0002.bin ${filesize}';
+
+	setenv update_primary_image2 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} u-boot0001.itb ${filesize}';
+	setenv update_secondary_image2 'echo "${fio_msg} writing ${image_path} ..."; mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr} u-boot0002.itb ${filesize}';
+	setenv set_primary_boot "multi_boot 1"
+	setenv set_secondary_boot "multi_boot 2"
+fi
 
 setenv check_board_closed "is_boot_authenticated"
 setenv check_secondary_boot "multi_boot"
-setenv set_primary_boot "multi_boot 1"
-setenv set_secondary_boot "multi_boot 2"
 
 @@INCLUDE_COMMON@@

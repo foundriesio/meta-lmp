foundriesio/meta-lmp

commit 4f77f5900cb66c54eb06170c54e379c5d071e4ac

base: add setting up of mxm mwifiex autoload
New NXP Wi-Fi driver uses a common dispatcher moal.ko to load
real wifi drivers using its configuration file, provided by
linux-firmware extension for imx-firmware. Add a recipie to
autoload this dispatcher with correct parameters on system boot.

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

meta-lmp-base/recipes-support/mxm-mwifiex-setup/mxm-mwifiex-setup_0.1.bb
@@ -1,0 +1,24 @@
+SUMMARY = "Setup NXP MXM MWiFiEx kernel module"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+
+inherit allarch
+
+SRC_URI = " \
+	file://modules-load-moal.conf \
+	file://modprobe-moal.conf \
+"
+
+S = "${WORKDIR}"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+do_install() {
+	install -d ${D}${libdir}/modules-load.d
+	install -m 0644 ${WORKDIR}/modules-load-moal.conf ${D}${libdir}/modules-load.d/moal.conf
+
+	install -d ${D}${libdir}/modprobe.d
+	install -m 0644 ${WORKDIR}/modprobe-moal.conf ${D}${libdir}/modprobe.d/moal.conf
+}
+
+FILES_${PN} += "${libdir}/modules-load.d/moal.conf ${libdir}/modprobe.d/moal.conf"

meta-lmp-base/recipes-support/mxm-mwifiex-setup/mxm-mwifiex-setup/modprobe-moal.conf
@@ -1,0 +1,2 @@
+# Parameters for NXP MXM MWiFiEx dispatcher module
+options moal mod_para=nxp/wifi_mod_para.conf drvdbg=0x6

meta-lmp-base/recipes-support/mxm-mwifiex-setup/mxm-mwifiex-setup/modules-load-moal.conf
@@ -1,0 +1,2 @@
+# NXP MXM MWiFiEx dispatcher module (serves all variants of 8987/8997)
+moal

commit 8df607c840255307c15059b8e6cd20fbaa0ec6d9

base: mxm-mwifiex-setup: use common license file
There is a common directory in LmP with license files. Use it instead
of shipped with meta-freescale.

Fixes: 4f77f590 ("base: add setting up of mxm mwifiex autoload")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

meta-lmp-base/recipes-support/mxm-mwifiex-setup/mxm-mwifiex-setup_0.1.bb
@@ -1,6 +1,6 @@
 SUMMARY = "Setup NXP MXM MWiFiEx kernel module"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 inherit allarch
 
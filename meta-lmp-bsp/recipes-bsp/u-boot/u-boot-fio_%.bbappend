foundriesio/meta-lmp

commit f83356f0c533607684cd3dd716f3db244a3fd501

bsp: fix single-char variables
Rename single-char variables to make code more understandable and
avoid possible name conflicts.

Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

meta-lmp-bsp/recipes-bsp/u-boot/u-boot-fio_%.bbappend
@@ -7,10 +7,10 @@ do_deploy_append_mx8() {
     # Deploy u-boot-nodtb.bin and XX.dtb for mkimage to generate boot binary
     if [ -n "${UBOOT_CONFIG}" ]; then
         for config in ${UBOOT_MACHINE}; do
-            i=$(expr $i + 1);
+            machine_idx=$(expr $machine_idx + 1);
             for type in ${UBOOT_CONFIG}; do
-                j=$(expr $j + 1);
-                if [ $j -eq $i ]; then
+                type_idx=$(expr $type_idx + 1);
+                if [ $type_id -eq $machine_idx ]; then
                     install -d ${DEPLOYDIR}/${BOOT_TOOLS}
                     # When sign is enabled the final DTB should be copied from deploy dir
                     if [ "${UBOOT_SIGN_ENABLE}" = "1" ]; then
@@ -22,9 +22,9 @@ do_deploy_append_mx8() {
                     install -m 0644 ${B}/${config}/u-boot-nodtb.bin ${DEPLOYDIR}/${BOOT_TOOLS}/u-boot-nodtb.bin-${MACHINE}-${UBOOT_CONFIG}
                 fi
             done
-            unset  j
+            unset type_idx
         done
-        unset  i
+        unset machine_idx
     else
         install -d ${DEPLOYDIR}/${BOOT_TOOLS}
         # When sign is enabled the final DTB should be copied from deploy dir

meta-lmp-bsp/recipes-bsp/u-boot/u-boot-fio-mfgtool_%.bbappend
@@ -7,10 +7,10 @@ do_deploy_append_mx8() {
     # Deploy u-boot-nodtb.bin and XX.dtb for mkimage to generate boot binary
     if [ -n "${UBOOT_CONFIG}" ]; then
         for config in ${UBOOT_MACHINE}; do
-            i=$(expr $i + 1);
+            machine_idx=$(expr $machine_idx + 1);
             for type in ${UBOOT_CONFIG}; do
-                j=$(expr $j + 1);
-                if [ $j -eq $i ]; then
+                type_idx=$(expr $type_idx + 1);
+                if [ $type_idx -eq $machine_idx ]; then
                     install -d ${DEPLOYDIR}/${BOOT_TOOLS}
                     # When sign is enabled the final DTB should be copied from deploy dir
                     if [ "${UBOOT_SIGN_ENABLE}" = "1" ]; then
@@ -22,9 +22,9 @@ do_deploy_append_mx8() {
                     install -m 0644 ${B}/${config}/u-boot-nodtb.bin ${DEPLOYDIR}/${BOOT_TOOLS}/u-boot-nodtb.bin-${MACHINE}-${UBOOT_CONFIG}
                 fi
             done
-            unset  j
+            unset type_idx
         done
-        unset  i
+        unset machine_idx
     else
         install -d ${DEPLOYDIR}/${BOOT_TOOLS}
         # When sign is enabled the final DTB should be copied from deploy dir

commit aeb2ab5dfe42de2afba221eedc03719a9e5639d2

bsp: u-boot-fio: Fix variable name typo in u-boot-fio_%.bbappend
Typo was introduced by commit f83356f0 (bsp: fix single-char variables,
2021-10-21)

Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

meta-lmp-bsp/recipes-bsp/u-boot/u-boot-fio_%.bbappend
@@ -10,7 +10,7 @@ do_deploy_append_mx8() {
             machine_idx=$(expr $machine_idx + 1);
             for type in ${UBOOT_CONFIG}; do
                 type_idx=$(expr $type_idx + 1);
-                if [ $type_id -eq $machine_idx ]; then
+                if [ $type_idx -eq $machine_idx ]; then
                     install -d ${DEPLOYDIR}/${BOOT_TOOLS}
                     # When sign is enabled the final DTB should be copied from deploy dir
                     if [ "${UBOOT_SIGN_ENABLE}" = "1" ]; then
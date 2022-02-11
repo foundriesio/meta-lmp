foundriesio/meta-lmp

commit eb0e3a58493ca16cb7e0f0e49bb7b0f82079664b

bsp: imx-boot: only support SPL ddr sign patch for 8mm/8mq
This patch somehow breaks the boot for mfgtools on 8mp and needs
more debugging.

Signed-off-by: Michael Scott <mike@foundries.io>

meta-lmp-bsp/dynamic-layers/freescale-layer/recipes-bsp/imx-mkimage/imx-boot_%.bbappend
@@ -4,8 +4,16 @@ DEPENDS_remove = "optee-os"
 DEPENDS += "${@bb.utils.contains('MACHINE_FEATURES', 'optee', 'virtual/optee-os', '', d)}"
 
 
-SRC_URI_append_mx8m = " \
+# TEMP: Need to figure out why this is breaking mfgtool builds on mx8mp
+# For now only apply the patch to mx8mm and mx8mq
+SRC_URL_append_mx8mm = " \
      file://0001-iMX8M-support-SPL-ddr-sign.patch \
+"
+SRC_URL_append_mx8mq = " \
+     file://0001-iMX8M-support-SPL-ddr-sign.patch \
+"
+
+SRC_URI_append_mx8m = " \
      file://0002-iMX8M-add-SPL-only-build.patch \
      file://0003-iMX8M-add-support-for-packing-HDMI-fw-in-SPL-only-bo.patch \
      file://0004-iMX8M-also-create-nohdmi-boot-image.patch \

commit ef94af5d0c2814e4bb236668d09de5a88b9f6fc4

bsp: imx-mkimage: fix typo
Change SRC_URL to SRC_URI.
This fixes the regression caused by eb0e3a58.

Signed-off-by: Vanessa Maegima <vanessa.maegima@foundries.io>

meta-lmp-bsp/dynamic-layers/freescale-layer/recipes-bsp/imx-mkimage/imx-boot_%.bbappend
@@ -8,10 +8,10 @@ SRCREV = "1112c88470f339dc631e2a7117087f416af6d6b5"
 
 # TODO: Need to figure out why this is breaking mfgtool builds on mx8mp
 # For now only apply the patch to mx8mm and mx8mq
-SRC_URL_append_mx8mm = " \
+SRC_URI_append_mx8mm = " \
      file://0001-iMX8M-support-SPL-ddr-sign.patch \
 "
-SRC_URL_append_mx8mq = " \
+SRC_URI_append_mx8mq = " \
      file://0001-iMX8M-support-SPL-ddr-sign.patch \
 "
 SRC_URI_append_mx8m = " \
01d06ae6bd23cf3fdd78e8a79365d606725bc9b7
Igor Opaniuk <igor.opaniuk@foundries.io>
2022-07-05 17:36:40 +0300

    bsp: u-boot-ostree-scr-fit: change index of rootfs partition
    As current layout includes additional fip partition, use different index
    for rootfs partition
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/stm32mp15-disco/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/stm32mp15-disco/boot.cmd
@@ -4,12 +4,13 @@ echo "Using ${fdtfile}"
 setenv bootlimit 3
 setenv devtype ${boot_device}
 setenv devnum ${boot_instance}
+setenv rootpart 6
 
 setenv loadaddr ${ramdisk_addr_r}
 
 setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
-setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:5 ${loadaddr} /boot/loader/uEnv.txt; env import -t ${loadaddr} ${filesize} kernel_image bootargs kernel_image2 bootargs2'
-setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:5 ${loadaddr} "/boot"${kernel_image}'
+setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:${rootpart} ${loadaddr} /boot/loader/uEnv.txt; env import -t ${loadaddr} ${filesize} kernel_image bootargs kernel_image2 bootargs2'
+setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:${rootpart} ${loadaddr} "/boot"${kernel_image}'
 setenv bootcmd_run 'bootm ${loadaddr}#conf-${fdtfile}'
 setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
 setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'

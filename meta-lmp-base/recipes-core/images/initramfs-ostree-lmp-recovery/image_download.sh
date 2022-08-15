0f2e179b2aa57fe4bdd1aeca6f3371bfee092a81
Ricardo Salveti <ricardo@foundries.io>
2022-08-15 20:53:46 -0300

    base: initramfs-ostree-lmp-recovery: add sample for image download
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

---
+++ meta-lmp-base/recipes-core/images/initramfs-ostree-lmp-recovery/image_download.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Copyright (C) 2022 Foundries.IO Ltd.
+# Licensed on MIT
+
+image_download_enabled() {
+	# Disabled by default, to be replaced based on the target hardware
+	return 1
+}
+
+image_download_run() {
+	token=`fw_printenv -n osf_token`
+	[ -z "$token" ] && fatal "Missing osf_token u-boot env definition"
+	source /etc/os-release
+	# Example for using wget to download via a token stored in u-boot env
+	wget --header="OSF-TOKEN: $token" https://api.foundries.io/projects/${LMP_FACTORY}/builds/${IMAGE_VERSION}/runs/${LMP_MACHINE}/lmp-factory-image-${LMP_MACHINE}.wic.gz
+}

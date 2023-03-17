89b8c6c565fe78d48b81c77d04bf43d3addb035a
Daiane Angolini <daiane.angolini@foundries.io>
2023-03-17 10:15:28 -0300

    bsp: u-boot-ostree-scr-fit: Add boot.cmd for imx93-11x11-lpddr4x-evk
    Add the boot.cmd file for u-boot-ostree-scr-fit.
    
    Signed-off-by: Daiane Angolini <daiane.angolini@foundries.io>

---
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/imx93-11x11-lpddr4x-evk/boot.cmd
@@ -0,0 +1,29 @@
+echo "Using freescale_${fdtfile}"
+
+# Default boot type and device
+setenv bootlimit 3
+setenv devtype mmc
+setenv devnum 0
+setenv bootpart 1
+setenv rootpart 2
+
+# Boot image files
+setenv fdt_file_final freescale_${fdtfile}
+setenv fit_addr 0x85000000
+
+# Boot firmware updates
+
+# Offsets are in blocks (512KB each)
+setenv bootloader 0x42
+setenv bootloader2 0x300
+setenv bootloader_s 0x1042
+setenv bootloader2_s 0x1300
+
+setenv bootloader_image "imx-boot"
+setenv bootloader_s_image ${bootloader_image}
+setenv bootloader2_image "u-boot.itb"
+setenv bootloader2_s_image ${bootloader2_image}
+setenv uboot_hwpart 1
+
+@@INCLUDE_COMMON_IMX@@
+@@INCLUDE_COMMON@@

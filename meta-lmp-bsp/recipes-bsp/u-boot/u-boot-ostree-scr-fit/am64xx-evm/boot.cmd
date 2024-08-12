b59d7b9c9c924c8fbe9cb039b95cbf558a41994e
Igor Opaniuk <igor.opaniuk@foundries.io>
2024-08-12 15:10:12 +0200

    bsp: u-boot-ostree-scr-fit: add boot firmware updates for am64xx-evm
    Add support for boot firmware updates for am64xx-evm machine.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

--- meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/am64xx-evm/boot.cmd
+++ meta-lmp-bsp/recipes-bsp/u-boot/u-boot-ostree-scr-fit/am64xx-evm/boot.cmd
@@ -2,24 +2,51 @@
 setenv bootlimit 3
 setenv devtype mmc
 setenv devnum ${mmcdev}
+setenv bootpart 1
+setenv rootpart 2
 setenv name_fdt ti/k3-am642-sk.dtb
 
 setenv loadaddr 0x90000000
+setenv fit_addr "${loadaddr}"
 
-setenv bootcmd_resetvars 'setenv kernel_image; setenv bootargs; setenv kernel_image2; setenv bootargs2'
-setenv bootcmd_otenv 'run bootcmd_resetvars; ext4load ${devtype} ${devnum}:2 ${scriptaddr} /boot/loader/uEnv.txt; env import -t ${scriptaddr} ${filesize} kernel_image bootargs kernel_image2 bootargs2'
-setenv bootcmd_load_f 'ext4load ${devtype} ${devnum}:2 ${loadaddr} "/boot"${kernel_image}'
-setenv bootcmd_run 'bootm ${loadaddr}#${name_fit_config}'
-setenv bootcmd_rollbackenv 'setenv kernel_image ${kernel_image2}; setenv bootargs ${bootargs2}'
-setenv bootcmd_set_rollback 'if test ! "${rollback}" = "1"; then setenv rollback 1; setenv upgrade_available 0; saveenv; fi'
-setenv bootostree 'run bootcmd_load_f; run findfdt; setenv fdtfile ${name_fdt}; run get_fit_config; run bootcmd_run'
-setenv altbootcmd 'run bootcmd_otenv; run bootcmd_set_rollback; if test -n "${kernel_image2}"; then run bootcmd_rollbackenv; fi; run bootostree; reset'
+setenv bootcmd_custom_run 'run findfdt; setenv fdtfile ${name_fdt}; run get_fit_config; bootm ${loadaddr}#${name_fit_config}'
 
-if test ! -e ${devtype} ${devnum}:1 uboot.env; then saveenv; fi
+setenv bootloader1_src_image "tiboot3-am64x-gp-evm.bin"
+setenv board_is_closed "0"
 
-# Boot firmware upgrade is not yet supported (set to 0 to unblock ak-lite)
-if test "${bootupgrade_available}" = "1"; then setenv bootupgrade_available 0; saveenv; fi
+if test "${device_type}" = "hs-fs"; then
+setenv bootloader1_src_image "tiboot3-am64x_sr2-hs-fs-evm.bin"
+setenv board_is_closed "0"
+fi
+if test "${device_type}" = "hs-se"; then
+setenv bootloader1_src_image "tiboot3-am64x_sr2-hs-evm.bin"
+setenv board_is_closed "1"
+fi
 
-if test "${rollback}" = "1"; then run altbootcmd; else run bootcmd_otenv; run bootostree; if test ! "${upgrade_available}" = "1"; then setenv upgrade_available 1; saveenv; fi; reset; fi
+setenv bootloader2_src_image "tispl.bin"
+setenv bootloader3_src_image "u-boot.img"
+setenv bootloader4_src_image "boot.itb"
 
-reset
+setenv bootloader1_image "tiboot3.bin"
+setenv bootloader2_image "${bootloader2_src_image}"
+setenv bootloader3_image "${bootloader3_src_image}"
+setenv bootloader4_image "${bootloader4_src_image}"
+
+setenv copy_img 'fatload ${devtype} ${devnum}:${bootpart} ${loadaddr} ${file_src} && setenv run_update "${update_cmd} ${file_dst} ${filesize}"; run run_update'
+setenv backup_primary_image 'echo "${fio_msg} backup primary boot image set ..."; setenv file_src ${bootloader1_image} && setenv file_dst ${bootloader1_image}.bck && run copy_img && setenv file_src ${bootloader2_image} && setenv file_dst ${bootloader2_image}.bck && run copy_img && setenv file_src ${bootloader3_image} && setenv file_dst ${bootloader3_image}.bck && run copy_img && setenv file_src ${bootloader4_image} && setenv file_dst ${bootloader4_image}.bck && run copy_img'
+setenv restore_primary_image 'echo "${fio_msg} restore primary boot image set ..."; setenv file_src ${bootloader1_image}.bck && setenv file_dst ${bootloader1_image} && run copy_img && setenv file_src ${bootloader2_image}.bck && setenv file_dst ${bootloader2_image} && run copy_img && setenv file_src ${bootloader3_image}.bck && setenv file_dst ${bootloader3_image} && run copy_img && setenv file_src ${bootloader4_image}.bck && setenv file_dst ${bootloader4_image} && run copy_img'
+setenv check_secondary_boot "setenv fiovb.is_secondary_boot 0"
+setenv check_board_closed "true"
+
+setenv setup_update 'setenv update_cmd "mmc dev ${devnum} && fatwrite mmc ${devnum}:${bootpart} ${loadaddr}"'
+setenv update_pri 'if test "${ostree_deploy_usr}" = "1"; then setenv image_path "${bootdir}/${bootloader_src_image}"; else setenv image_path "${ostree_root}/usr/lib/firmware/${bootloader_src_image}"; fi; run load_image && setenv run_update "${update_cmd} ${bootloader_image} ${filesize}"; echo "${fio_msg} update image ${bootloader_image}, using ${image_path} ..."; run run_update'
+
+setenv update_primary_image 'setenv bootloader_image ${bootloader1_image} && setenv bootloader_src_image ${bootloader1_src_image} && run update_pri && setenv bootloader_image ${bootloader2_image} && setenv bootloader_src_image ${bootloader2_src_image} && run update_pri && setenv bootloader_image ${bootloader3_image} && setenv bootloader_src_image ${bootloader3_src_image} && run update_pri && setenv bootloader_image ${bootloader4_image} && setenv bootloader_src_image ${bootloader4_src_image} && run update_pri'
+
+setenv do_reboot "reset"
+
+if test ! -e ${devtype} ${devnum}:${bootpart} uboot.env; then saveenv; fi
+
+run setup_update
+
+@@INCLUDE_COMMON_ALTERNATIVE@@
